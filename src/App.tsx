f0230b80130c2a9a0375a7c40498c1387dcc0679
B.Cheolu <dev.cheolubak@gmail.com>
2022-10-17 21:02:19 +0900

    기본 프로젝트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import { RecoilRoot } from 'recoil';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const HomePage = React.lazy(() => import('./components/pages/Home'));
+const QuizPage = React.lazy(() => import('./components/pages/Quiz'));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <Suspense>
+        <HomePage />
+      </Suspense>
+    ),
+  },
+  {
+    path: '/quiz',
+    element: (
+      <Suspense>
+        <QuizPage />
+      </Suspense>
+    ),
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+      <RouterProvider router={router} />
+    </RecoilRoot>
   );
 }
 
